049a55f6c3eb4519d06e0828342bf72f2671f928
suryo <suryoatm@gmail.com>
2021-08-28 12:35:40 +0700

    add insert mitra

---
+++ tentang.php
@@ -0,0 +1,59 @@
+<?php
+echo ("ekampung.id");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+<title>EKampungn</title>
+</head>
+<body>
+
+<h1>Tentang Ekampung</h1>
+<p>Selamat Datang di Ekampung</p>
+  
+<?php
+include 'header.php';
+?>
+
+<h3>
+  <font color = darkgrey>
+KAMPUNG berdasarkan KBBI merupakan kelompok rumah yang merupakan bagian kota ; bisa disebut juga desa atau dusun; kesatuan administrasi terkecil yang menempati wilayah tertentu, terletak di bawah kecamatan; 
+biasanya terkebelakang (belum modern); berkaitan dengan kebiasaan di kampung; kolot;
+-- halaman daerah atau desa tempat kelahiran
+</font>
+</h3>
+<h2>
+  <font color = rainbow>
+Ekampung merupakan media berbasis digital untuk mewujudkan SDGs desa.
+Sustainable Development Goals (SDGs) merupakan suatu rencana aksi global yang disepakati oleh para pemimpin dunia, 
+termasuk Indonesia, guna mengakhiri kemiskinan, mengurangi kesenjangan dan melindungi lingkungan. 
+SDGs berisi 17 Tujuan dan 169 Target yang diharapkan dapat dicapai pada tahun 2030
+</font>
+</h2>
+
+<h1>
+ekampung memiliki misi : mewujudkan percepatan pencapaian tujuan pembangunana nasional berkelanjutan berbasis platform digital
+</h1>
+ <footer>
+  <p>Author: SuryoAtmojo</p>
+  <p><a href="mailto:suryoatm@gmail.com">ekampung.id</a></p>
+</footer> 
+</body>
+</html> 

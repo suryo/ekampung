deea39040a614ef8a245349960367890f4f46c63
suryo <suryoatm@gmail.com>
2021-11-02 14:13:19 +0700

    modif koneksi

--- koneksi.php
+++ koneksi.php
@@ -8,5 +8,9 @@
   if(!$koneksi){ //jika tidak terkoneksi maka akan tampil error
     die ("Koneksi dengan database gagal: ".mysql_connect_error());
   }
+  // else
+  // {
+  //   echo("terkoneksi gaes");
+  // }
  
 ?>
